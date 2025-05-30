402e6c770d8c44799a00a36924046087a6c120c5
Alexander Silva Lima <lima.alexander.las@gmail.com>
2025-05-30 10:06:29 -0300

    "Adicionado o pdf dos exerícios e a solução do exercício 3"

---
+++ MediaNumeros.java
@@ -0,0 +1,13 @@
+public class MediaNumeros{
+    public static void main(String []args){
+        
+        int numero1, numero2, numero3, resultado;
+        numero1 = 5;
+        numero2 = 9;
+        numero3 = 85;
+
+        resultado = (numero1 + numero2 + numero3) / 3;
+
+        System.out.println("Resultado: "+resultado);
+    }
+}
