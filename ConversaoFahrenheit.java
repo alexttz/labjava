a8bec2e6133abcbfd0859bc09222ca000bd84de8
Alexander Silva Lima <lima.alexander.las@gmail.com>
2025-05-30 12:01:03 -0300

    "Correção ortográfica no ConversaoFahrenheit"

--- ConversaoFahrenheit.java
+++ ConversaoFahrenheit.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class ConversaoFarenheit{
+public class ConversaoFahrenheit{
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
         
         System.out.println("Digite a temperatura em Celsius: ");
         double celsius = input.nextDouble();
 
-        double farenheit = ((9*celsius)/5) + 32;
+        double fahrenheit = ((9*celsius)/5) + 32;
 
-        System.out.println("Temperatura em Farenheit: "+farenheit);
+        System.out.println("Temperatura em Farenheit: "+fahrenheit);
     }
 }
