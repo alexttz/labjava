3a817fe73409e6684d3db92d910a9aaee08397ec
Alexander Silva Lima <lima.alexander.las@gmail.com>
2025-05-30 10:00:08 -0300

    "Adicionado arquivos da primeira lista de exercicios do curso da Udemy - Java (Básico ao avançado)

---
+++ CalculoQuadrado.java
@@ -0,0 +1,9 @@
+public class CalculoQuadrado{
+    public static void main(String []args){
+        double numero = 8.2;
+
+        double quadradoResultado = numero * numero;
+
+        System.out.println("Resultado: " + quadradoResultado);
+    }
+}
---
+++ Soma.java
@@ -0,0 +1,11 @@
+public class Soma{
+    public static void main(String []args){
+        
+        int n1 = 10;
+        int n2 = 5;
+        
+        int resultado = n1 + n2;
+
+        System.out.print("Resultado: " + resultado);
+    }
+}
