dbc190e18eeca4b5a4d417c38034aa1d63249f06
Alexander Silva Lima <lima.alexander.las@gmail.com>
2025-05-30 10:18:14 -0300

    "Adicionado a resolução do exercício 4, (PositivoNegativo)"

--- PositivoNegativo.java
+++ PositivoNegativo.java
@@ -1,6 +1,14 @@
 public class PositivoNegativo{
     public static void main(String[]args){
 
-        
+        int numero = 5;
+
+        if(numero < 0){
+            System.out.println("O numero é negativo! ");
+        }else if(numero > 0){
+            System.out.println("O numero é positivo! ");
+        }else{
+            System.out.println("O numero é igual a zero. ");
+        }
     }
 }
