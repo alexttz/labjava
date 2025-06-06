07909a24185cd366066b9d71ba9a9995b875862b
Alexander Silva Lima <lima.alexander.las@gmail.com>
2025-06-06 11:36:24 -0300

    "Adicionado o arquivo de estudo usando loops em Java, LoopComWhile.java"

---
+++ LoopComWhile.java
@@ -0,0 +1,14 @@
+public class LoopComWhile {
+    public static void main(String []args){
+      
+        int i = 10;
+
+        while(i >= 0){
+            System.out.println(i);
+            i--;
+        }
+    }
+}
+
+
+/*Código básico que demonstra o uso de fluxos de repetição em java */
