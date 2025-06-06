0760097d707ccfc8303ecdabb73f9d39b0bf26d5
Alexander Silva Lima <lima.alexander.las@gmail.com>
2025-06-06 11:43:52 -0300

    "Ajustado LoopComWhile para receber os dados pelo teclado"

--- LoopComWhile.java
+++ LoopComWhile.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class LoopComWhile {
     public static void main(String []args){
-      
-        int i = 10;
+
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Digite o número desejado: ");
+        int i = teclado.nextInt();
 
         while(i >= 0){
             System.out.println(i);
             i--;
         }
+
+        teclado.close();
     }
 }
 
