e27b1b2480b03a01acf4235bfad230b6caa33fcf
Alexander Silva Lima <lima.alexander.las@gmail.com>
2025-06-06 10:49:33 -0300

    "Adicionado o arquivo do programa para calculo de maior idade (fora da lista de exercícios)"

---
+++ CalculaIdade.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CalculaIdade {
+    public static void main (String []args){
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite a sua idade: ");
+        int idade = teclado.nextInt();
+
+        if(idade > 18 && idade < 60){
+            System.out.println("Você já é maior de idade! ");
+        }else if (idade > 60 ){
+            System.out.println("Você é idoso! ");
+        }else{
+            System.out.println("Você é menor de idade! ");
+        }
+
+        teclado.close();
+    }
+}
