5bbc344fd3e7e70c3d4e742b7fd2df38def2e221
Alexander Silva Lima <lima.alexander.las@gmail.com>
2025-06-09 11:25:54 -0300

    "Adicionado o arquivo ContadorPares.java"

---
+++ ContadorPares.java
@@ -0,0 +1,10 @@
+public class ContadorPares {
+    public static void main(String []args){
+
+        int numero = 100;
+
+        for(int i = 0; i <= numero; i += 2){
+            System.out.println(i);
+        }
+    }    
+}
