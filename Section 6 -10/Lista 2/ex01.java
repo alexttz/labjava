f5e45df3a24f1169e31ccc9c29abf6e551f3e5de
Alexander Silva Lima <lima.alexander.las@gmail.com>
2025-06-11 11:44:46 -0300

    "Adicionado a resolução do ex01 da Lista 2"

--- Section 6 -10/Lista 2/ex01.java
+++ Section 6 -10/Lista 2/ex01.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 
 public class ex01{
-
     public static void main(String[] args) {
         
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite um número de 1 a 10:");
+        int numero = teclado.nextInt();
+
+        while(numero > 0){
+            System.out.println(numero);
+            numero--;        
+        }
+        teclado.close();
     }
 }
