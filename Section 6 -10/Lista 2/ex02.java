d24b31fbd63739bf02ca56af537c54d80709454c
Alexander Silva Lima <lima.alexander.las@gmail.com>
2025-06-11 12:22:12 -0300

    "Adicionado a resolução do ex02 da Lista 2"

---
+++ Section 6 -10/Lista 2/ex02.java
@@ -0,0 +1,13 @@
+public class ex02 {
+    public static void main (String[] args){
+        
+        int indice = 0;
+        int soma = 0;
+
+        while(indice < 100){
+            soma = soma + (indice + 1);
+            System.out.println(soma);
+            indice++;
+        }
+    }    
+}
