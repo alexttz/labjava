f029ad45a3335c92e2da601089139803a8d38a81
alexttz <lima.alexander.las@gmail.com>
2025-08-18 17:44:46 -0300

    "Implementação dos exercícios 3 e 4 da lista 2"

---
+++ Section 6 -10/Lista 2/ex04.java
@@ -0,0 +1,9 @@
+public class ex04 {
+    public static void main(String[] args) {
+        for(int i = 1; i <=50; i++){
+            if (i > 1 && i % 2 == 1) {
+                System.out.println("O número " + i + " é primo");
+            }
+        }
+    }
+}
